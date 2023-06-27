b53d80500ad28e51d77c61b8263fffad78c280bf
pravinbhagwat <pravinbhagwat150@gmail.com>
2023-06-27 16:01:06 +0530

    submitting code-Tue Jun 27 2023 16:01:06 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,30 @@
 
 import React from "react";
+import { useState,useEffect } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const [data, setdata] = useState([]);
+
+  useEffect(() => {
+     function fetchData() {
+      fetch("https://dummyjson.com/products")
+      .then((res)=>(res.json()))
+      .then((res)=>{setdata(res)})
+      .catch((err)=>{console.log(err); setdata(`An error occurred: ${err}`)})
+    }
+    fetchData();
+  }, []);
+
+  if (!data) {
+    return <p>Loading product data...</p>;
+  }
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="app">
+    <h1>Data Fetched from API</h1>
+        <pre className="data">{JSON.stringify(data,null,2)}</pre>
     </div>
   )
 }
